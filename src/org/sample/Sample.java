fdba541e089b54c11ee829b3fb03753f2bf4e6fc
Elamathy21 <elamathyforever@gmail.com>
2021-03-26 12:51:41 +0530

    added to B

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,9 +1,11 @@
 package org.sample;
 
 public class Sample {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Java Samplecode");
+		String name = "Greens";
+		System.out.println(name);
 	}
 
 }
